a11cfeb3a8b3dac8bdcbe5d6bbd7b3713393b046
jbuchholzer-rrc <jbuchholzer@academic.rrc.ca>
2025-09-16 09:55:48 -0500

    feat:  add initial Health endpoint and updated pkg.json

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+/**
+ * File: src/app.ts
+ * Purpose: main express app for assignment-1.
+ * Author: Jack Buchholzer
+ */
+
+
+
+
+// Import express and types for handling requests and responses
+import express, {Request, Response} from 'express';
+// imprt package.json to get version info
+import pkg from "../package.json";
+
+
+// String literal type for health status
+export type Healthstatus = "OK";
+
+
+// shape interface for json response from healthcheck endpoint
+export interface HealthResponse {
+    status: Healthstatus; // curent health status (string literal type)
+    uptime: number;  // server uptime (number type)
+    timestamp: number; // current timestamp (number type)
+    version: string; // version from package.json (string type)
+
+
+}
+
+
+// Create and export the express app
+export const app = express();
+
+
+// Create GET route for healthcheck endpoint
+
+app.get("/api/v1/health", (_req: Request, res: Response<HealthResponse>) => {
+    const body: HealthResponse = {
+        status: "OK",
+        uptime: process.uptime(),
+        timestamp: Date.now(),
+        version: pkg.version
+    };
+    res.json(body);
+
+    res.status(200).json(body);
+
+
+
+});
+
+export default app;
+
